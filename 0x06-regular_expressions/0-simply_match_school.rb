723b0ff456c72842b86fec29e7c1111e71298560
Kiche Carolyne <kichec@gmail.com>
2023-08-01 15:46:03 -0700

    regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-# regex matcher
-
-regex = /School/
-
-  if match_result
-    puts match_result[0]
-  else
-    puts "No match found."
-  end
-
-p "School is awesome".match regex
-p "I love going to School".match regex
-p "I was at School by eight".match regex
+puts ARGV[0].scan(/Holberton/).join
