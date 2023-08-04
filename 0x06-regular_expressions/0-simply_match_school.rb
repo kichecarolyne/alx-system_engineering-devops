1d20d390f856c5110125e107c193f1c2e43c59ac
Kiche Carolyne <kichec@gmail.com>
2023-08-04 05:43:29 -0700

    regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-#regex matcher
-
-def regex = 'School\n'
-
-  if match_result
-  puts ARGV[0].match(/School/).join
-  else
-  puts "No match found."
-end
-
-"School is awesome".match regex
-"I love going to School".match regex
-"I was at School by eight".match regex
-
+puts ARGV[0].scan(/School/).join
